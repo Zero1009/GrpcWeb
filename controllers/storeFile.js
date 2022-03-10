0e96fbb2e0d9265a9983df3383f34f5bd4ffd20d
admin <Landslot20>
2022-03-10 17:29:12 +0700

    uploadFile

--- controllers/storeFile.js
+++ controllers/storeFile.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
 
-const storeFile = (req, res) => {
-  try {
-    res.json({ message: 'Hi world' });
-  } catch (error) {}
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads/'+file.originalname.split('.')[1]);
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  });
+  
+const uploadImg = multer({ storage: storage }).single('image');
+  
+const test = (req,res)=>{
+  console.log(storage.diskStorage)
+  res.send('Success')
 };
-
-module.exports = { storeFile };
+module.exports = {  uploadImg ,test};
